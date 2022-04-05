00954293351bc69773ca166d64d847f9c34693c9
furaihan <28698437+furaihan@users.noreply.github.com>
2022-04-05 22:59:35 +0700

    initial commit

---
+++ Numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int a, int b)
+        {
+            return getMin(a, b);
+        }
+        public int FindMinimum(int a, int b, float c)
+        {
+            return getMin(a, b, (int)c);
+        }
+        public int FindMaximum(int a, int b)
+        {
+            return getMax(a, b);
+        }
+        public int FindMaximum(int a, int b, float c)
+        {
+            return getMax(a, b, (int)c);
+        }
+        private static int getMax(params int[] arr)
+        {
+            return arr.ToList().OrderBy(x => x).Last();
+        }
+        private static int getMin(params int[] arr)
+        {
+            return arr.ToList().OrderBy(x => x).First();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LatihanOverloadingMethod
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+            Console.WriteLine($"Minimum #1: {number.FindMinimum(5, 125)}");
+            Console.WriteLine($"Minimum #2: {number.FindMinimum(70, 128, 22)}");
+            Console.WriteLine();
+            Console.WriteLine($"Maximum #1: {number.FindMaximum(6, 120)}");
+            Console.WriteLine($"Maximum #2: {number.FindMaximum(70, 800, 92)}");
+        }
+    }
+}
